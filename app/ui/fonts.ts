2e01fe374ae5410f45dd1bdb99376b34dff3855f
Alan GR <alan3805@gmail.com>
2024-02-29 23:42:27 -0600

    Finish chapter 9

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Montserrat, Lusitana } from 'next/font/google';
+
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const montserrat = Montserrat({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+});
